53b9ebd5a28a230375ad35e3a53a9e24db610957
Ilkka Vesa <25800334+ilkkavesa@users.noreply.github.com>
2018-04-22 21:12:54 +0700

    Profile and Team updates

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,7 +12,14 @@ class Profile extends React.Component {
       return (
         <div>
           <p>Profile</p>
-          <p>{this.props.data.loggedInUser.id}</p>
+          <p>ID: {this.props.data.user.id}</p>
+          <p>Email: {this.props.data.user.email}</p>
+          <p>First name: {this.props.data.user.firstName}</p>
+          <p>Last name: {this.props.data.user.lastName}</p>
+          <p>Username: {this.props.data.user.userName}</p>
+          <p>Protile photo url: {this.props.data.user.profilePhoto}</p>
+          <p>Created at: {this.props.data.user.createdAt}</p>
+          <p>Updated at: {this.props.data.user.updatedAt}</p>
         </div>
       );
     }
@@ -20,19 +27,21 @@ class Profile extends React.Component {
 
 } // end of class
 
-const LOGGED_IN_USER_QUERY = gql`
-  query LoggedInUserQuery {
-    loggedInUser {
+const CURRENT_USER_QUERY = gql`
+  query CurrentUserQuery {
+    user {
       id
-      # firstName
-      # lastName
-      # userName
-      # createdAt
-      # updatedAt
+      email
+      firstName
+      lastName
+      userName
+      createdAt
+      updatedAt
+      profilePhoto
     }
   }
 `
 
 export default compose(
-  graphql(LOGGED_IN_USER_QUERY)
+  graphql(CURRENT_USER_QUERY)
 )(Profile)
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,13 +1,53 @@
 import React from 'react';
 
-export default class Tem extends React.Component {
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import List from 'material-ui/List';
+import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+
+class Team extends React.Component {
 
   render () {
-    return (
-      <div>
-        <p>Team...</p>
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    } else {
+      return (
+        <div>
+          <p>Team:</p>
+          <List>
+            {
+          this.props.data.allUsers.map((teamMember, index) => (
+            <ListItem>
+              <ListItemText>
+                ID: {teamMember.id}
+              </ListItemText>
+            </ListItem>
+          ))
+        }
+        </List>
       </div>
-    )
+      );
+    }
   }
 
 } // end of class
+
+const ALL_USERS_QUERY = gql`
+  query AllUserQuery {
+    allUsers {
+      id
+      email
+      firstName
+      lastName
+      userName
+      createdAt
+      updatedAt
+      profilePhoto
+    }
+  }
+`
+
+export default compose(
+  graphql(ALL_USERS_QUERY)
+)(Team)
