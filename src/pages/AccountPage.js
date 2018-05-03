f9df13be5f2b3620becf24db64eeb15847022cda
Ilkka Vesa <25800334+ilkkavesa@users.noreply.github.com>
2018-05-03 23:18:14 +0700

    account

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -3,9 +3,28 @@ import React from 'react';
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
-import TextField from 'material-ui/TextField';
-import Button from 'material-ui/Button';
-import Avatar from 'material-ui/Avatar';
+import { TextField, Button, Avatar, IconButton } from 'material-ui';
+
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import PersonIcon from '@material-ui/icons/Person';
+import UsernameIcon from '@material-ui/icons/PersonOutline';
+import EmailIcon from '@material-ui/icons/Email';
+
+// Privacy icons:
+import OnlyMeIcon from '@material-ui/icons/Lock';
+import TeamIcon from '@material-ui/icons/SupervisorAccount';
+import PublicIcon from '@material-ui/icons/Public';
+
+import List, {
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+} from 'material-ui/List';
+
 
 import { printDate } from '../components/functions.js';
 
@@ -64,7 +83,82 @@ class AccountPage extends React.Component {
               style={{height: 200, width: 200}}
           />
           </div>
-          <div>          
+
+          <div style={styles.avatarContainer}>          
+            <List style={{width: 350}}>              
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <PersonIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary='First name'
+                  secondary={this.props.data.user.firstName ? this.props.data.user.firstName : 'Missing...'}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton aria-label="Delete">
+                    <PublicIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>                
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <PersonIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary='Last name'
+                  secondary={this.props.data.user.lastName ? this.props.data.user.lastName : 'Missing...'}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton aria-label="Delete">
+                    <PublicIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>                
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <UsernameIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary='Username'
+                  secondary={this.props.data.user.userName ? this.props.data.user.userName : 'Missing...'}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton aria-label="Delete">
+                    <TeamIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>                
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <EmailIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary='Email'
+                  secondary={this.props.data.user.email ? this.props.data.user.email : 'Missing...'}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton aria-label="Delete">
+                    <OnlyMeIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>                            
+              <ListItem>
+                <ListItemText
+                  primary='Joined'
+                  secondary={this.props.data.user.createdAt ? printDate(this.props.data.user.createdAt) : 'Missing...'}
+                />
+              </ListItem>                            
+            </List>
+          </div>
+          {/* <div>          
             <form noValidate autoComplete="off">
               <TextField
                 autoFocus={true}
@@ -105,7 +199,8 @@ class AccountPage extends React.Component {
             </form>
           </div>
           {this._renderSaveButton()}
-          <p>Joined: {printDate(this.props.data.user.createdAt)}</p>
+          <p>Joined: {printDate(this.props.data.user.createdAt)}</p> */}
+
         </div>
       );
     }
